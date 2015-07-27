8b39a22704d786479dd1b15839ebd2d4cf52151d
Fergus Strange <fergusstrange@gmail.com>
2015-07-27 23:14:33 +0100

    Health controller for deployment.

---
+++ web/src/main/java/com/nakedgardener/web/HealthOKController.java
@@ -0,0 +1,24 @@
+package com.nakedgardener.web;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+public class HealthOKController {
+
+    @ResponseBody
+    @RequestMapping(value = "/", method = GET, produces = APPLICATION_JSON_VALUE)
+    public OKStatus healthCheck() {
+        return new OKStatus();
+    }
+
+    private class OKStatus {
+        @JsonProperty
+        private final String status = "OK";
+    }
+}
