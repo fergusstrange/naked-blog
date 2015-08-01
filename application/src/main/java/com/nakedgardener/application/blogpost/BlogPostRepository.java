9239de1a9a8f0dcde73d432f04bedbbafaa899de
Fergus Strange <fergusstrange@gmail.com>
2015-08-01 11:27:54 +0100

    Remove unused code.

--- application/src/main/java/com/nakedgardener/application/blogpost/BlogPostRepository.java
+++ application/src/main/java/com/nakedgardener/application/blogpost/BlogPostRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public interface BlogPostRepository extends MongoRepository<BlogPost, String>, BlogPostRepositoryCustom {
+public interface BlogPostRepository extends MongoRepository<BlogPost, String> {
     BlogPost findByBlogPostSlug(String blogPostSlug);
     List<BlogPost> findByPostDateBefore(LocalDateTime date, Pageable pageable);
 }
--- application/src/main/java/com/nakedgardener/application/blogpost/BlogPostRepositoryCustom.java
+++
@@ -1,4 +0,0 @@
-package com.nakedgardener.application.blogpost;
-
-public interface BlogPostRepositoryCustom {
-}
