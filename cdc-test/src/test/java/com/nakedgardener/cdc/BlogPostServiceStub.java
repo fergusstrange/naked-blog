cd508a9c090d1e0f2dae447bdf1765f2a5032660
Fergus Strange <fergusstrange@gmail.com>
2015-08-03 11:39:38 +0100

    Mock out CDC test.

--- cdc-test/src/test/java/com/nakedgardener/cdc/BlogPostServiceStub.java
+++ cdc-test/src/test/java/com/nakedgardener/cdc/BlogPostServiceStub.java
@@ -6,13 +6,23 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 @Component
 @Profile("stubbed")
 public class BlogPostServiceStub implements BlogPostService {
 
     @Override
     public ResponseEntity<BlogPost> findByBlogPostSlug(String blogPostSlug) {
-        return ResponseEntity.ok(new BlogPost());
+        return ResponseEntity.ok(
+                new BlogPost(
+                        "55ac30b0936d190ae717e317",
+                        LocalDateTime.of(2014, 2, 2, 1, 9),
+                        blogPostSlug,
+                        "A Test Blog",
+                        "Some content in the blog!"
+                )
+        );
     }
 
     @Override
---
+++ cdc-test/src/test/java/com/nakedgardener/cdc/CDCApplication.java
@@ -0,0 +1,9 @@
+package com.nakedgardener.cdc;
+
+import com.nakedgardener.web.Application;
+
+public class CDCApplication {
+    public static void main(String... args) {
+        Application.main();
+    }
+}
