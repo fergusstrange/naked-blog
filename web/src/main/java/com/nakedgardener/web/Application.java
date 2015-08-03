de6d8ee8b2b52696ff9b23e43ae932dd0e1669f1
Fergus Strange <fergusstrange@gmail.com>
2015-08-03 14:56:16 +0100

    Stop auto configuration happening for Mongo.

--- web/src/main/java/com/nakedgardener/web/Application.java
+++ web/src/main/java/com/nakedgardener/web/Application.java
@@ -1,12 +1,16 @@
 package com.nakedgardener.web;
 
 import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.nakedgardener")
+@EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class Application {
     public static void main(String... args) {
         new SpringApplicationBuilder()
