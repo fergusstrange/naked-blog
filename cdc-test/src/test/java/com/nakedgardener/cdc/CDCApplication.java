cce0a94da65493760f929ef1e6c5f692f4edc63f
Fergus Strange <fergusstrange@gmail.com>
2015-08-03 11:54:38 +0100

    Remove unused code.

--- cdc-test/src/test/java/com/nakedgardener/cdc/CDCApplication.java
+++
@@ -1,9 +0,0 @@
-package com.nakedgardener.cdc;
-
-import com.nakedgardener.web.Application;
-
-public class CDCApplication {
-    public static void main(String... args) {
-        Application.main();
-    }
-}
