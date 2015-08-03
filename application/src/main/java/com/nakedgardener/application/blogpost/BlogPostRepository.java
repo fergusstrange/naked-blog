5c97dc3c29e18936621461cf66213614aef67176
Fergus Strange <fergusstrange@gmail.com>
2015-08-03 15:58:34 +0100

    Unused.

--- application/src/main/java/com/nakedgardener/application/blogpost/BlogPostRepository.java
+++ application/src/main/java/com/nakedgardener/application/blogpost/BlogPostRepository.java
@@ -1,14 +1,12 @@
 package com.nakedgardener.application.blogpost;
 
 import com.nakedgardener.application.domain.BlogPost;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Profile("!stubbed")
 public interface BlogPostRepository extends MongoRepository<BlogPost, String> {
     BlogPost findByBlogPostSlug(String blogPostSlug);
     List<BlogPost> findByPostDateBefore(LocalDateTime date, Pageable pageable);
