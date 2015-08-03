71f1bb25f2b3c7dbcf53cbb4af1827695857297c
Fergus Strange <fergusstrange@gmail.com>
2015-08-03 11:57:19 +0100

    Create interface to back the recent blog post service.

---
+++ application/src/main/java/com/nakedgardener/application/recentblogpost/RecentBlogPostServiceImpl.java
@@ -0,0 +1,51 @@
+package com.nakedgardener.application.recentblogpost;
+
+import com.nakedgardener.application.blogpost.BlogPostRepository;
+import com.nakedgardener.application.domain.BlogPost;
+import com.nakedgardener.application.domain.BlogPosts;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static com.nakedgardener.application.domain.BlogPosts.blogPostsBuilder;
+import static com.nakedgardener.application.domain.BlogPosts.emptyBlogPosts;
+import static java.time.LocalDateTime.now;
+import static org.springframework.data.domain.Sort.Direction.DESC;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.ResponseEntity.ok;
+
+@Component
+@Profile("!stubbed")
+public class RecentBlogPostServiceImpl implements RecentBlogPostService {
+
+    private final BlogPostRepository blogPostRepository;
+    private final Logger applicationErrorLog;
+
+    @Autowired
+    public RecentBlogPostServiceImpl(BlogPostRepository blogPostRepository, Logger applicationErrorLog) {
+        this.blogPostRepository = blogPostRepository;
+        this.applicationErrorLog = applicationErrorLog;
+    }
+
+    @Override
+    public ResponseEntity<BlogPosts> recentBlogPosts(int page, int pageSize) {
+        try {
+            Pageable pageable = new PageRequest(page, pageSize, DESC, "postDate");
+            List<BlogPost> blogPosts = blogPostRepository.findByPostDateBefore(now(), pageable);
+            return ok(blogPostsBuilder()
+                    .blogPosts(blogPosts)
+                    .build()
+            );
+        }
+        catch(Exception e) {
+            applicationErrorLog.error("An error has occurred in the RecentBlogPostServiceImpl", e);
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(emptyBlogPosts());
+        }
+    }
+}
