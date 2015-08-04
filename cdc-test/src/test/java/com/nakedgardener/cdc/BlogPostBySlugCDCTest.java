ad164f97a85a4e99f3d8a2cd17a08fe4d19c1c55
Fergus Strange <fergusstrange@gmail.com>
2015-08-04 15:47:37 +0100

    Rewrite some of the pact broker to always return json instead of html. This has allowed the @PactFile annotation to actually be used.

--- cdc-test/src/test/java/com/nakedgardener/cdc/BlogPostBySlugCDCTest.java
+++ cdc-test/src/test/java/com/nakedgardener/cdc/BlogPostBySlugCDCTest.java
@@ -9,8 +9,10 @@ import com.reagroup.pact.provider.PactRunner;
 import com.reagroup.pact.provider.ProviderState;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
 import java.time.LocalDateTime;
 
@@ -19,9 +21,9 @@ import static org.mockito.Matchers.anyString;
 
 @ActiveProfiles("stubbed")
 @RunWith(PactRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
-@PactFile("url:http://lianli:5555/pacts/provider/naked-blog/consumer/naked-gardener/latest")
-public class CDCTestRunner {
+@ContextConfiguration(classes = Application.class)
+@PactFile("http://lianli:5555/pact/provider/naked-blog/consumer/naked-gardener/latest/json")
+public class BlogPostBySlugCDCTest {
 
     @Autowired
     private BlogPostController blogPostController;
@@ -29,8 +31,10 @@ public class CDCTestRunner {
     @Autowired
     private BlogPostRepository blogPostRepository;
 
-    @ProviderState("blog-post-by-slug")
-    public BlogPostController myServiceForbidsARequestWithInvalidToken() throws Exception {
+    @ProviderState("A blog post with slug la-la-la")
+    public BlogPostController returnsValidRequestWithBody() throws Exception {
+        Resource resource = new DefaultResourceLoader().getResource("http://lianli:5555/pacts/provider/naked-blog/consumer/naked-gardener/latest");
+        resource.getInputStream();
         given(blogPostRepository.findByBlogPostSlug(anyString())).willReturn(new BlogPost(
                 "55ac30b0936d190ae717e317",
                 LocalDateTime.of(
@@ -40,10 +44,12 @@ public class CDCTestRunner {
                         1,
                         9
                 ),
-                "slugy-slug-slug",
+                "la-la-la",
                 "A Test Blog",
                 "Some content in the blog!"
         ));
         return blogPostController;
     }
+
+
 }
